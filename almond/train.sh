220e1544bf6564fcdc7b91654ffc0defa27de0ff
Almond Lambda <lambda@almondbot.com>
2025-04-29 17:15:09 +0000

    add git setup

--- almond/train.sh
+++ almond/train.sh
@@ -1,8 +1,13 @@
 TASK=$1
 
+git config --global user.email "lambda@almondbot.com"
+git config --global user.name "Almond Lambda"
+
 sudo apt update && sudo apt upgrade -y
 wget -qO- https://astral.sh/uv/install.sh | sh
+
 uv sync --extra "almond" --extra "pi0"
+
 sudo apt install zstd -y
 wget -O ~/almond/zed_v5.run https://download.stereolabs.com/zedsdk/5.0/cu12/ubuntu22
 sudo chmod +x ~/almond/zed_v5.run
