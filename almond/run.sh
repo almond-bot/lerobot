0f25f40438acbcce72f8a29dd0a0a6de288b53c8
Almond Jetson <jetson@almondbot.com>
2025-05-01 14:39:45 -0700

    update run script

--- almond/run.sh
+++ almond/run.sh
@@ -1,12 +1,24 @@
-MODEL=$1
+TASK=$1
 
-if [ -z "$MODEL" ]; then
-    echo "Error: Model not specified"
+if [ -z "$TASK" ]; then
+    echo "Error: Task not specified"
     exit 1
 fi
 
+rm -fr ~/.cache/huggingface/lerobot/shawnptl8/eval_pi0_${TASK}
+
 uv run lerobot/scripts/control_robot.py \
     --robot.type=almond \
-    --control.type=teleoperate \
+    --control.type=record \
     --control.fps=20 \
-    --control.policy.path=outputs/train/${MODEL}/checkpoints/last/pretrained_model
+    --control.repo_id=shawnptl8/eval_pi0_${TASK} \
+    --control.warmup_time_s=15 \
+    --control.episode_time_s=30 \
+    --control.reset_time_s=15 \
+    --control.num_episodes=1 \
+    --control.push_to_hub=false \
+    --control.num_image_writer_processes=0 \
+    --control.num_image_writer_threads_per_camera=0 \
+    --control.display_data=false \
+    --control.play_sounds=false \
+    --control.policy.path=outputs/train/pi0_${TASK}/checkpoints/last/pretrained_model
