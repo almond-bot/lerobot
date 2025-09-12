3e6d4693dd8e36a818876e9da410c4c554ea3b3c
Almond Workstation <workstation@almondbot.com>
2025-09-12 12:21:36 -0500

    update calibrate script to use leader arm

--- almond/calibrate.py
+++ almond/calibrate.py
@@ -1,15 +1,40 @@
-from env import FOLLOWER_PORT
+import argparse
+
+from env import FOLLOWER_PORT, LEADER_PORT
 
 from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
+from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
 
 
-def main():
+def calibrate_leader():
+    config = SO101LeaderConfig(port=LEADER_PORT)
+    leader = SO101Leader(config)
+
+    leader.connect(calibrate=False)
+    leader.calibrate()
+    leader.disconnect()
+
+
+def calibrate_follower():
     config = SO101FollowerConfig(port=FOLLOWER_PORT)
-
     follower = SO101Follower(config)
+
     follower.connect(calibrate=False)
     follower.calibrate()
     follower.disconnect()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--leader", action="store_true", help="Calibrate the leader arm")
+    parser.add_argument("--follower", action="store_true", help="Calibrate the follower arm")
+
+    args = parser.parse_args()
+
+    if args.leader:
+        calibrate_leader()
+    if args.follower:
+        calibrate_follower()
 
 
 if __name__ == "__main__":
--- almond/configure.py
+++ almond/configure.py
@@ -6,29 +6,31 @@ from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
 from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
 
 
-def calibrate_leader():
+def configure_leader():
     config = SO101LeaderConfig(port=LEADER_PORT)
     leader = SO101Leader(config)
+
     leader.setup_motors()
 
 
-def calibrate_follower():
+def configure_follower():
     config = SO101FollowerConfig(port=FOLLOWER_PORT)
     follower = SO101Follower(config)
+
     follower.setup_motors()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--leader", action="store_true", help="Calibrate the leader arm")
-    parser.add_argument("--follower", action="store_true", help="Calibrate the follower arm")
+    parser.add_argument("--leader", action="store_true", help="Configure the leader arm")
+    parser.add_argument("--follower", action="store_true", help="Configure the follower arm")
 
     args = parser.parse_args()
 
     if args.leader:
-        calibrate_leader()
+        configure_leader()
     if args.follower:
-        calibrate_follower()
+        configure_follower()
 
 
 if __name__ == "__main__":
