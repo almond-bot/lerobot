f2981913f1111f9d25e79e49462850334c3b30c1
Almond Workstation <workstation@almondbot.com>
2025-09-12 13:07:07 -0500

    auth with huggingface

--- almond/setup.sh
+++ almond/setup.sh
@@ -1,3 +1,13 @@
 uv sync --extra feetech --extra almond
 uv tool install pre-commit
 pre-commit install
+
+git config --global credential.helper store
+git config --global user.email "workstation@almondbot.com"
+git config --global user.name "Almond Workstation"
+
+echo "Please enter your Hugging Face login token:"
+read HUGGINGFACE_TOKEN
+export HUGGINGFACE_TOKEN
+
+hf auth login --token ${HUGGINGFACE_TOKEN} --add-to-git-credential
