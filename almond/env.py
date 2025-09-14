13e3a33736bc0cc32f439af588f8f39a944972f8
Shawn Patel <shawnptl8@gmail.com>
2025-09-13 21:25:55 -0500

    update training pipeline

--- almond/env.py
+++ almond/env.py
@@ -9,6 +9,11 @@ load_dotenv(f"{DIR_NAME}/.env.local", override=True)
 
 HF_USER = os.environ["HF_USER"]
 
+LOGTAIL_HOST = os.environ["LOGTAIL_HOST"]
+LOGTAIL_TOKEN = os.environ["LOGTAIL_TOKEN"]
+
+SLACK_ENG_OPERATIONS_URL = os.environ["SLACK_ENG_OPERATIONS_URL"]
+
 LEADER_PORT = os.environ["LEADER_PORT"]
 FOLLOWER_PORT = os.environ["FOLLOWER_PORT"]
 OVERHEAD_CAM_PORT = os.environ["OVERHEAD_CAM_PORT"]
