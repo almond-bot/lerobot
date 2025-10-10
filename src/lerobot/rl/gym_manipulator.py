3f338838faffb4235efa62e64f2ca6e0ee066197
Almond Workstation <workstation@almondbot.com>
2025-10-10 16:39:17 -0500

    change gripper speed factor back to 1.0

--- src/lerobot/rl/gym_manipulator.py
+++ src/lerobot/rl/gym_manipulator.py
@@ -501,7 +501,7 @@ def make_processors(
             ),
             GripperVelocityToJoint(
                 clip_max=cfg.processor.max_gripper_pos,
-                speed_factor=0.25,
+                speed_factor=1.0,
                 discrete_gripper=True,
             ),
             InverseKinematicsRLStep(
